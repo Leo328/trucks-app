425f53ef2283eb3b6adbfab6af26954bfc212a3f
Matt Lane <lane.matthew@gmail.com>
2018-04-18 20:26:44 -0700

    avoiding object spread

--- src/TruckList.js
+++ src/TruckList.js
@@ -4,7 +4,9 @@ import "./TruckList.css";
 
 const TruckList = ({ truckData }) => (
   <div className="TruckList">
-    {truckData.map((d, i) => <Truck key={i} {...d} />)}
+    {truckData.map((d, i) => (
+      <Truck key={i} type={d.type} rating={d.rating} src={d.src} />
+    ))}
   </div>
 );
 
